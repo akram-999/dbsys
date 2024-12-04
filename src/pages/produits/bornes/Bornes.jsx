a26d6be774ef95c815c7585c283ebfcbe079f362
akram ahil <akram.ahil123@gmail.com>
2024-12-04 11:37:26 +0100

    num

--- src/pages/produits/bornes/Bornes.jsx
+++ src/pages/produits/bornes/Bornes.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import Breadcrumb from '../../../components/crump/Breadcrumb'
+// import Breadcrumb from '../../../components/crump/Breadcrumb'
+import Numers from './number'
 
 export default function Bornes() {
   return (
     <div>
-      <Breadcrumb pageName="BORNES"/>
+      <div class="relative overflow-hidden">
+            <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10">
+              
+
+              <div class="mt-10 relative max-w-5xl mx-auto">
+                <div class="w-full object-cover h-96 sm:h-[480px] bg-[url('https://dbsys-services.ma/img/parallax/kiosk.jpg')] bg-no-repeat bg-center bg-cover rounded-xl" data-aos="flip-up"></div>
+
+                
+
+                <div class="absolute bottom-12 -start-20 -z-[1] size-48 bg-gradient-to-b from-yellow-400 to-white p-px rounded-lg dark:to-neutral-900">
+                  <div class=" size-48 rounded-lg "></div>
+                </div>
+
+                <div class="absolute -top-12 -end-20 -z-[1] size-48 bg-gradient-to-t from-yellow-400 dark:to-gray-900 p-px rounded-full">
+                  <div class=" size-48 rounded-full "></div>
+                </div>
+              </div>
+
+              <div class="max-w-2xl mt-5 text-center mx-auto" data-aos="zoom-out-down">
+                <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl dark:text-white">LES BORNES DE COMMANDE</h1>
+                <p class="mt-3 text-lg text-gray-800 dark:text-neutral-400 ">Que ce soit pour l’intérieur ou l’extérieur, les bornes de commande à écran tactile sont conçues pour augmenter le confort client et mieux gérer les flux de consommateurs lors des heures de grande affluence.</p>
+                <p className='dark:text-neutral-400'>Notre gamme de bornes libre-service a été conçue pour s’adapter à de nombreux secteurs d’activité, tel que : la restauration rapide, l’hôtellerie, les loisirs et le commerce de détail.</p>
+              </div>
+            </div>
+          </div>
+
+          <Numers/>
     </div>
   )
 }
---
+++ src/pages/produits/bornes/number.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { useSpring, animated } from 'react-spring'
+
+
+
+function Number ({n}) {
+ const {number} = useSpring({
+    from : {number : 0},
+    number : n, 
+    delay:200,
+    config:{mass:1,tension:20,friction:10},
+ });
+ return <animated.div className='text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-yellow-400'>{number.to((n)=>n.toFixed(0))}</animated.div>
+}
+function Number2 ({n}) {
+ const {number} = useSpring({
+    from : {number : 0},
+    number : n, 
+    delay:200,
+    config:{mass:1,tension:20,friction:10},
+ });
+ return <animated.div className='text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-yellow-400'>{number.to((n)=>n.toFixed(0))}</animated.div>
+}
+
+function Number3 ({n}) {
+ const {number} = useSpring({
+    from : {number : 0},
+    number : n, 
+    delay:200,
+    config:{mass:1,tension:20,friction:10},
+ });
+ return <animated.div className='text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-yellow-400'>{number.to((n)=>n.toFixed(0))}</animated.div>
+}
+
+function Number4 ({n}) {
+ const {number} = useSpring({
+    from : {number : 0},
+    number : n, 
+    delay:200,
+    config:{mass:1,tension:20,friction:10},
+ });
+ return <animated.div className='text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-yellow-400'>{number.to((n)=>n.toFixed(0))}</animated.div>
+}
+
+
+
+export default function number() {
+
+  return (
+    <section class="py-10 bg-gray-100 sm:py-16 lg:py-24">
+    <div class="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-2xl mx-auto text-center" data-aos="fade-down">
+            <h3 class="text-2xl font-bold leading-tight text-black sm:text-4xl lg:text-4xl">Chez certains de <span className='underline decoration-yellow-400'>nos clients</span></h3>
+           </div>
+
+        <div class="grid grid-cols-1 gap-8 mt-10 text-center lg:mt-24 sm:gap-x-8 md:grid-cols-4">
+            <div data-aos="zoom-in">
+                <h3 class=" font-bold text-7xl">
+                    <span class="flex ml-9 text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-yellow-400"> <Number n={20} /> %</span>
+                </h3>
+                <p class="mt-4 text-xl font-medium text-gray-900">VENTES DE BOISSONS</p>
+                
+            </div>
+
+            <div data-aos="zoom-in">
+                <h3 class="font-bold text-7xl">
+                    <span class="flex ml-9 text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-yellow-400"> <Number2 n={30} /> % </span>
+                </h3>
+                <p class="mt-4 text-xl font-medium text-gray-900">TICKET MOYEN</p>
+                
+            </div>
+
+            <div data-aos="zoom-in">
+                <h3 class="font-bold text-7xl">
+                    <span class="flex ml-9 text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-yellow-400"> <Number3 n={50} /> % </span>
+                </h3>
+                <p class="mt-4 text-xl font-medium text-gray-900">VENTES SUPPLÉMENTAIRES</p>
+                
+            </div>
+
+            <div data-aos="zoom-in">
+                <h3 class="font-bold text-7xl">
+                    <span class="flex ml-9 text-transparent bg-clip-text bg-gradient-to-r from-orange-300 to-yellow-400"> <Number4 n={50} /> %  </span>
+                </h3>
+                <p class="mt-4 text-xl font-medium text-gray-900">HAUSSE DES VENTES</p>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+  )
+}
